56df1ab0d7fc56b57609088e6b1fa397288793a6
Nell Junior <nelljunior@hotmail.com>
2026-09-14 20:55:29 -0300

    incluido novamente os pngs relevantes para este projeto

--- vendor/js/chat.js
+++ vendor/js/chat.js
@@ -228,7 +228,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 var imagem = document.querySelector('.chat-avatar');
 
                 if (imagem) {
-                    imagem.src = '/img/' + avatar + '.png';
+                    imagem.src = './img/' + avatar + '.png';
                 }
             }
         })
